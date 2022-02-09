80de5455af2f78d7ccaedeb21ab0ecf5a45d5abb
wowbaseballgamesocool <99157059+wowbaseballgamesocool@users.noreply.github.com>
2022-02-09 08:46:47 -0500

    Update updategame.py

--- updategame.py
+++ updategame.py
@@ -1,22 +1,29 @@
 #update check
 import urllib, requests, os, zipfile
 folderpath = os.getcwd()
+
+# check updates
+
 try: 
     response = requests.get("https://api.github.com/repos/wowbaseballgamesocool/baseballgame/releases/latest")
     latestversion = response.json()["tag_name"].strip("v")
-except: 
-    print("Could not check for updated version (check internet connection)")
+except Exception as e:
+    if "Max retries exceeded with url" in str(e):
+        print("Could not check for updated version (check internet connection)  [Max retries exceeded]")
+    else: print("Could not check for updated version\nError: " + str(e))
     latestversion = version
     internet = False
 
 if str(version) != str(latestversion):
     print("New update, restart game after download finishes")
+    print("downloading...")
     url = "https://github.com/wowbaseballgamesocool/baseballgame/releases/download/" + str(latestversion) + "//Baseball.Game.zip"
     urllib.request.urlretrieve(url, filename = folderpath + r"//Baseball.Game.zip")
     with zipfile.ZipFile(folderpath + "\\Baseball.Game.zip", 'r') as zip_ref:
         os.rename(folderpath + "\\Baseball_game.exe", folderpath + "\\gamefiles\\Old_Baseball_game.exe")
         zip_ref.extractall(folderpath)
         zip_ref.close()
+    #os.remove(folderpath + "\\gamefiles\\Old_Baseball_game.exe")
     os.remove(folderpath + "\\Baseball.Game.zip")
     exit()
 else: 
